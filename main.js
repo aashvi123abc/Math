cc03eb43e72ea3814102d66e080ac882130aac84
aashvi123abc <80174888+aashvi123abc@users.noreply.github.com>
2021-05-08 13:22:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name="Aashvi";
+var last_name="Nandan";
+var a=10;
+var b=5;
+var added= a + b;
+console.log("10 plus 5 is equal to ",added)
+var subtracted= a-b
+console.log("10 minus 5 is equal to ",subtracted)
+var multiplied= a*b
+console.log("10 multiplied by 5 is equal to:",multiplied)
+var divided= a/b
+console.log("10 divided by 5 is equal to ",divided)
